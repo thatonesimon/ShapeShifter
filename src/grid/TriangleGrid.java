46e7efe95495ba0efac7ea6794f6d2999eb437ae
Simon Wong <swong@amazon.com>
2018-11-08 13:12:28 -0800

    Triangle grid somewhat working

--- src/grid/TriangleGrid.java
+++ src/grid/TriangleGrid.java
@@ -1,5 +1,6 @@
 package grid;
 
+import color.Colors;
 import model.Drawing;
 import model.PAppletController;
 import processing.core.PApplet;
@@ -18,9 +19,8 @@ public class TriangleGrid extends PAppletController implements Drawing {
     }
 
     public void setup() {
-
+        noStroke();
         setupPoints();
-
     }
 
     int numX = 30;
@@ -35,21 +35,81 @@ public class TriangleGrid extends PAppletController implements Drawing {
     public void draw() {
         background(0);
         for(int i = 0; i < numX; i++) {
-            for(int j = 0; j < numY+2; j++) {
-                PVector p = points[numX][numY];
-                ellipse(p.x, p.y, 10, 10);
+            for(int j = 0; j < numY+1; j++) {
+                if(points[i][j] != null) {
+                    PVector p = points[i][j];
+                    // ellipse(p.x, p.y, 10, 10);
+                }
             }
         }
 
+        for(int i = 0; i < numX-1; i++) {
+            for(int j = 0; j < numY + 2; j++) {
+
+                try {
+
+                    if(i%2 == 0) {
+                    // if(i%2 == 1) {
+                        fill(Colors.BLACK);
+                        beginShape();
+                        //    3c
+                        // 1a
+                        //    2b
+                        vertex(points[i][j].x, points[i][j].y);
+                        vertex(points[i+1][j+1].x, points[i+1][j+1].y);
+                        vertex(points[i+1][j].x, points[i+1][j].y);
+                        endShape(CLOSE);
+
+                        fill(Colors.WHITE);
+                        beginShape();
+                        // 1a
+                        //    2b
+                        // 3c
+                        vertex(points[i][j].x, points[i][j].y);
+                        vertex(points[i+1][j+1].x, points[i+1][j+1].y);
+                        vertex(points[i][j+1].x, points[i][j+1].y);
+                        endShape(CLOSE);
+                    } else {
+                        fill(Colors.WHITE);
+
+                        beginShape();
+                        // 1a
+                        //    3c
+                        // 2b
+                        vertex(points[i][j].x, points[i][j].y);
+                        vertex(points[i][j+1].x, points[i][j+1].y);
+                        vertex(points[i+1][j].x, points[i+1][j].y);
+                        endShape(CLOSE);
+
+                        fill(Colors.BLACK);
+                        beginShape();
+                        //    1c
+                        // 2b
+                        //    3d
+                        vertex(points[i+1][j].x, points[i+1][j].y);
+                        vertex(points[i][j+1].x, points[i][j+1].y);
+                        vertex(points[i+1][j+1].x, points[i+1][j+1].y);
+                        endShape(CLOSE);
+                    }
+
+
+                } catch (Exception e) {
+                    println("issue with " + i + ", " + j);
+                }
+
+
+
+            }
+        }
     }
 
+    // TODO: setup with i, j and not x, y
     public void setupPoints() {
 
         int col = 0;
         for(float x = 0; x < width; x+=triHeight) {
             int row = 0;
-            for(float y = col%2==0 ? 0 : triSide/2.0f; y < height+triSide/2.0f; y+=triSide) {
-
+            for(float y = col%2==0 ? triSide/2.0f : 0; y < height+triSide/2.0f; y+=triSide) {
                 points[col][row] = new PVector(x, y);
                 row++;
             }
