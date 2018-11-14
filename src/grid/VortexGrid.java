6d567bdcb954f06281d8d2f3300a568132c28584
Simon Wong <swong@amazon.com>
2018-11-14 12:26:44 -0800

    Removed unused code

--- src/grid/VortexGrid.java
+++ src/grid/VortexGrid.java
@@ -7,28 +7,11 @@ public class VortexGrid extends SquareGrid {
 
     public VortexGrid(PApplet pApplet) {
         super(pApplet);
-        // gravity.add(new PVector(radius*cos(radians(millis()))+width/2.0f, radius*sin(radians(millis()))+height/2.0f));
-        // gravity.add(new PVector(2*radius*cos(radians(millis()))+width/2.0f, 2*radius*sin(radians(millis()))+height/2.0f));
-        originalDist = new float[points.length][points[0].length];
-        PVector c = new PVector(width/2.0f, height/2.0f);
-        for(int i = 0; i < originalDist.length; i++) {
-            for (int j = 0; j < originalDist[0].length; j++) {
-                originalDist[i][j] = c.dist(points[i][j]);
-            }
-        }
-        originalAngle = new float[points.length][points[0].length];
-        for(int i = 0; i < originalAngle.length; i++) {
-            for (int j = 0; j < originalAngle[0].length; j++) {
-                originalAngle[i][j] = atan2(points[i][j].y-c.y,points[i][j].x-c.x);
-            }
-        }
     }
 
     public void setup() {
         noStroke();
     }
-
-    int radius = 200;
 
     public void draw() {
         super.draw();
@@ -41,17 +24,11 @@ public class VortexGrid extends SquareGrid {
             for (int j = 0; j < points[0].length; j++) {
                 PVector p = points[i][j];
                 float angle = atan2(-p.y+center.y, -p.x+center.x);
-                float dist = originalDist[i][j];
-
+                float dist = p.dist(center);
                 angle = angle+dist/400.f*radians(1);
                 p.x = width/2.0f+dist*cos(angle);
                 p.y = height/2.0f+dist*sin(angle);
             }
         }
     }
-
-    float[][] originalDist;
-    float[][] originalAngle;
-
-
 }
